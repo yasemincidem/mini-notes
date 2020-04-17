05b8385b35ba8d94b7e209aaab35d0a0aaa1fccd
yasemincidem <yasemincidem@gmail.com>
2020-04-17 22:30:46 +0300

    auto expand was added

--- content.js
+++ content.js
@@ -12,6 +12,10 @@ function getSelectionDimensions() {
   return {left, top, height};
 }
 
+function autoExpand(field) {
+  field.style.height = field.scrollHeight + 'px';
+}
+
 document.addEventListener('mouseup', function () {
   var selectedText = getSelectionText();
   if (selectedText.length > 0) { // check there's some text selected
@@ -31,12 +35,33 @@ document.addEventListener('mouseup', function () {
     document.getElementById('selectedText').addEventListener('click', function (ev) {
       document.getElementById('selectedText').remove();
       const contentBoxContainer = document.createElement('div');
-      contentBoxContainer.innerHTML = `<div id="commentBox" style="left: ${left}px; top: ${topOfSelectedText}px" />`;
+      contentBoxContainer.innerHTML = `<div id="commentBox" style="left: ${left}px; top: ${topOfSelectedText}px">
+                                            <textarea id="commentTextBox"
+                                                      autocomplete="input-area"
+                                                      x-webkit-speech="" 
+                                                      speech=""
+                                                      role="combobox" 
+                                                      aria-autocomplete="list" 
+                                                      aria-label="Yorum taslağı">
+                                            </textarea>
+                                            <div id="buttonGroup">
+                                                      <div role="button" id="commentBtn" style="user-select: none;" aria-disabled="false" aria-label="Submit" tabindex="0">Submit</div>
+                                                      <div role="button" id="cancelBtn" tabindex="0" aria-label="Cancel" style="user-select: none;">Cancel</div>
+                                            </div>
+                                       </div>`;
       document.body.prepend(contentBoxContainer);
+      document.getElementById('commentTextBox').focus();
+      document.getElementById('commentTextBox').addEventListener('input', function (event) {
+        if (event.target.tagName.toLowerCase() !== 'textarea') return;
+        autoExpand(event.target);
+      }, false);
+      document.getElementById('cancelBtn').addEventListener('click', function (event) {
+        document.getElementById('commentBox').remove();
+      }, false);
     });
   } else {
-    if (document.getElementById('commentBox')) {
-      document.getElementById('commentBox').remove();
-    }
+    // if (document.getElementById('commentBox')) {
+    //   document.getElementById('commentBox').remove();
+    // }
   }
 }, false);
