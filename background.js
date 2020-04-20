4ba2e700581c7c216588cb63fe4ea2641a80e615
yasemincidem <yasemincidem@gmail.com>
2020-04-20 22:52:13 +0300

    redundant files removed

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-  console.log('oninstalled');
-  chrome.storage.local.get('notes', function(result) {
-    console.log('notes', result.notes);
-  });
-});
